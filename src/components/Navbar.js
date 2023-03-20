a0bd58375c9fed726efb832c593dce731106598b
21besart <sadikubesart11@gmail.com>
2023-03-20 14:15:53 +0100

    add navbar responsivity

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,6 +8,7 @@ export const Navbar = () => {
       direction="row"
       justifyContent="space-around"
       sx={{
+        overflowX: "auto",
         gap: { sm: "122px", xs: "40px" },
         mt: { sm: "32px", xs: "20px" },
         justifyContent: "none",
