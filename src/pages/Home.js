4915d558ee1e4ea733675b7d3200f93476237c1d
Besart Sadiku <sadikubesart11@gmail.com>
2024-02-19 23:37:26 +0100

    Fix import statement in Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import { Box } from "@mui/material";
 
 import { Exercises } from "../components/Exercises";
 import { HeroBanner } from "../components/HeroBanner";
-import { SearchExercises } from "../components/SearchExercises";
+import SearchExercises from "../components/SearchExercises";
 
 export const Home = () => {
   const [bodyPart, setBodyPart] = useState("all");
